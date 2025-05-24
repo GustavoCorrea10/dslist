88ef707f5d6de90f7db9f6a08487572e7d76d471
GustavoCorrea10 <guganow11@gmail.com>
2025-05-23 22:35:02 -0300

    GET game by id

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.dslist.entities.Game;
 
 public interface GameRepository extends JpaRepository<Game, Long>{
-
 	
 }
+
